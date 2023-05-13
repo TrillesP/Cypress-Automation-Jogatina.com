eeecbc3242fb85e66728da9ed10e86fb1f15765f
Pedro Trilles Machado <trillessky@gmail.com>
2023-05-13 15:10:24 -0300

    added comments

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -2,7 +2,7 @@ Cypress.Commands.add("isFixtureImg",{prevSubject: true},(ele, fixtureImg) =>{
     cy.wrap(ele)
         .should(([img]) => {
             expect(img.complete).to.be.true;
-        })
+        })    // expect utilizado para que o teste aguarde o browser carregar a imagem
         .then(([img]) => {
             cy.fixture(fixtureImg).then(image => {
                 let fixtureImg =  new Image();
@@ -11,7 +11,8 @@ Cypress.Commands.add("isFixtureImg",{prevSubject: true},(ele, fixtureImg) =>{
                     fixtureImg.onload = () => {
                         expect(img.naturalWidth).to.equal(fixtureImg.naturalWidth);
                         expect(img.naturalHeight).to.equal(fixtureImg.naturalHeight);
-                        resolve();
+                        //verifica tamanho da imagem
+                        resolve();   
                     }
                 })
             })
